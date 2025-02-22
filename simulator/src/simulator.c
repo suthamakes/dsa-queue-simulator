bfabb19bc30eb30aa0a61f8b9922528249bafe81
suthamakes <51185426+suthamakes@users.noreply.github.com>
2025-02-22 19:26:15 +0545

    Tested vehicle motion animation

--- simulator/src/simulator.c
+++ simulator/src/simulator.c
@@ -31,31 +31,63 @@ void receive_data(int sock) {
     } LanePosition;
 
 LanePosition lanePositions[4][3] = {
-    // A road lanes (A1, A2, A3)
-    { {150, 250, 0, 150}, {250, 350, 0, 150}, {350, 450, 0, 150} }, //check y_end imp!!!
+    // A road lanes (North to South) (A1, A2, A3)
+    { {150, 250, -30, -30}, {250, 350, -30, -30}, {350, 450, -30, -30} },
     
-    // B road lanes (B1, B2, B3)
-    {{350, 450, 450, 600}, {250, 350, 450, 600}, {150, 250, 450, 600}},
+    // B road lanes (South to North) (B1, B2, B3)
+    {{350, 450, 630, 630}, {250, 350, 630, 630}, {150, 250, 630, 630}},
     
-    // C road lanes (C1, C2, C3)
-    { {450, 600, 150, 250}, {450, 600, 250, 350}, {450, 600, 350, 450} },
+    // C road lanes (East to West) (C1, C2, C3)
+    { {630, 630, 150, 250}, {630, 630, 250, 350}, {630, 630, 350, 450} },
     
-    // D road lanes (D1, D2, D3)
-    {  {0, 150, 350, 450}, {0, 150, 250, 350},{0, 150, 150, 250} }
+    // D road lanes (West to East) (D1, D2, D3)
+    { {-30, -30, 350, 450}, {-30, -30, 250, 350}, {-30, -30, 150, 250} }
 };
-   
+
 void getLaneCenter(char road, int lane, int *x, int *y) {
     int roadIndex = road - 'A';  // Convert 'A'-'D' to index 0-3
     int laneIndex = lane - 1;    // Convert 1-3 to index 0-2
-
-    *x = (lanePositions[roadIndex][laneIndex].x_start + lanePositions[roadIndex][laneIndex].x_end) / 2 - 20 / 2;
-    *y = (lanePositions[roadIndex][laneIndex].y_start + lanePositions[roadIndex][laneIndex].y_end) / 2 - 20 / 2;
-
+    
+    // For horizontal roads (D and C)
+    if (road == 'D') {
+        *x = -30;  // Start beyond left edge
+        *y = (lanePositions[roadIndex][laneIndex].y_start + lanePositions[roadIndex][laneIndex].y_end) / 2 - 20 / 2;
+    }
+    else if (road == 'C') {
+        *x = SCREEN_WIDTH + 10;  // Start beyond right edge
+        *y = (lanePositions[roadIndex][laneIndex].y_start + lanePositions[roadIndex][laneIndex].y_end) / 2 - 20 / 2;
+    }
+    // For vertical roads (A and B)
+    else if (road == 'A') {
+        *x = (lanePositions[roadIndex][laneIndex].x_start + lanePositions[roadIndex][laneIndex].x_end) / 2 - 20 / 2;
+        *y = -30;  // Start beyond top edge
+    }
+    else if (road == 'B') {
+        *x = (lanePositions[roadIndex][laneIndex].x_start + lanePositions[roadIndex][laneIndex].x_end) / 2 - 20 / 2;
+        *y = SCREEN_HEIGHT + 10;  // Start beyond bottom edge
+    }
 }
 
 void moveVehicle(Vehicle *vehicle) {
     int targetX, targetY;
-    getLaneCenter(vehicle->targetRoad, vehicle->targetLane, &targetX, &targetY);
+    
+    // Set target positions beyond screen edges based on target road
+    if (vehicle->targetRoad == 'A') {
+        targetX = (lanePositions[0][vehicle->targetLane - 1].x_start + lanePositions[0][vehicle->targetLane - 1].x_end) / 2 - 20 / 2;
+        targetY = -30;  // Move beyond top edge
+    }
+    else if (vehicle->targetRoad == 'B') {
+        targetX = (lanePositions[1][vehicle->targetLane - 1].x_start + lanePositions[1][vehicle->targetLane - 1].x_end) / 2 - 20 / 2;
+        targetY = SCREEN_HEIGHT + 10;  // Move beyond bottom edge
+    }
+    else if (vehicle->targetRoad == 'C') {
+        targetX = SCREEN_WIDTH + 10;  // Move beyond right edge
+        targetY = (lanePositions[2][vehicle->targetLane - 1].y_start + lanePositions[2][vehicle->targetLane - 1].y_end) / 2 - 20 / 2;
+    }
+    else if (vehicle->targetRoad == 'D') {
+        targetX = -30;  // Move beyond left edge
+        targetY = (lanePositions[3][vehicle->targetLane - 1].y_start + lanePositions[3][vehicle->targetLane - 1].y_end) / 2 - 20 / 2;
+    }
 
     int reachedX = (abs(vehicle->rect.x - targetX) <= vehicle->speed);
     int reachedY = (abs(vehicle->rect.y - targetY) <= vehicle->speed);
@@ -64,20 +96,20 @@ void moveVehicle(Vehicle *vehicle) {
         (vehicle->road_id == 'B' && vehicle->targetRoad == 'D')) {
         // Move Y first (for A3 → C1 and B3 → D1)
         if (!reachedY) {
-            if (vehicle->rect.y < targetY) vehicle->rect.y += vehicle->speed;  // Move down
-            else vehicle->rect.y -= vehicle->speed;  // Move up
+            if (vehicle->rect.y < targetY) vehicle->rect.y += vehicle->speed;
+            else vehicle->rect.y -= vehicle->speed;
         } else if (!reachedX) {
-            if (vehicle->rect.x < targetX) vehicle->rect.x += vehicle->speed;  // Move right
-            else vehicle->rect.x -= vehicle->speed;  // Move left
+            if (vehicle->rect.x < targetX) vehicle->rect.x += vehicle->speed;
+            else vehicle->rect.x -= vehicle->speed;
         }
     } else {
         // Default: Move X first (for D3 → A1 and C3 → B1)
         if (!reachedX) {
-            if (vehicle->rect.x < targetX) vehicle->rect.x += vehicle->speed;  // Move right
-            else vehicle->rect.x -= vehicle->speed;  // Move left
+            if (vehicle->rect.x < targetX) vehicle->rect.x += vehicle->speed;
+            else vehicle->rect.x -= vehicle->speed;
         } else if (!reachedY) {
-            if (vehicle->rect.y < targetY) vehicle->rect.y += vehicle->speed;  // Move down
-            else vehicle->rect.y -= vehicle->speed;  // Move up
+            if (vehicle->rect.y < targetY) vehicle->rect.y += vehicle->speed;
+            else vehicle->rect.y -= vehicle->speed;
         }
     }
 }
@@ -104,32 +136,46 @@ int main() {
     // connect_to_server(sock, "127.0.0.1");
 
     Vehicle vehicle1 = {
-    {0, 0, 20, 20},  // Temporary position (updated below)
-    1, 'B', 3, 2,    // ID=1, starts at road 'D', lane 3, speed=3
-    'D', 1          // Target is road 'A', lane 1
+    {0, 0, 20, 20},
+    1, 'B', 3, 2,
+    'D', 1
     };
     getLaneCenter(vehicle1.road_id, vehicle1.lane, &vehicle1.rect.x, &vehicle1.rect.y);
 
     Vehicle vehicle2 = {
-    {600, 600, 20, 20},  // Temporary values (updated below)
-    2, 'A', 3, 2,  // ID=2, starts at road 'A', lane 3
-    'C', 1                     // Target is road 'C', lane 1
+    {600, 600, 20, 20},
+    2, 'A', 3, 2,
+    'C', 1
     };
     getLaneCenter(vehicle2.road_id, vehicle2.lane, &vehicle2.rect.x, &vehicle2.rect.y);
 
     Vehicle vehicle3 = {
-    {0, 0, 20, 20},  // Temporary position (updated below)
-    1, 'C', 3, 2,    // ID=1, starts at road 'D', lane 3, speed=3
-    'B', 1          // Target is road 'A', lane 1
+    {0, 0, 20, 20},
+    1, 'C', 3, 2,
+    'B', 1
     };
     getLaneCenter(vehicle3.road_id, vehicle3.lane, &vehicle3.rect.x, &vehicle3.rect.y);
 
     Vehicle vehicle4 = {
-    {600, 600, 20, 20},  // Temporary values (updated below)
-    2, 'D', 3, 2,  // ID=2, starts at road 'A', lane 3
-    'A', 1                     // Target is road 'C', lane 1
+    {600, 600, 20, 20},
+    2, 'D', 3, 2,
+    'A', 1
     };
     getLaneCenter(vehicle4.road_id, vehicle4.lane, &vehicle4.rect.x, &vehicle4.rect.y);
+
+    Vehicle vehicle5 = {
+    {0, 0, 20, 20},
+    1, 'D', 2, 2,
+    'C', 2
+    };
+    getLaneCenter(vehicle5.road_id, vehicle5.lane, &vehicle5.rect.x, &vehicle5.rect.y);
+
+    Vehicle vehicle6 = {
+    {600, 600, 20, 20},
+    2, 'A', 2, 3,
+    'B', 2
+    };
+    getLaneCenter(vehicle6.road_id, vehicle6.lane, &vehicle6.rect.x, &vehicle6.rect.y);
 
     int running = 1;
     SDL_Event event;
@@ -143,6 +189,8 @@ int main() {
         moveVehicle(&vehicle2);
         moveVehicle(&vehicle3);
         moveVehicle(&vehicle4);
+        moveVehicle(&vehicle5);
+        moveVehicle(&vehicle6);
         
         DrawBackground(renderer);
         TrafficLightState(renderer);
@@ -150,6 +198,8 @@ int main() {
         drawVehicle(renderer, &vehicle2);
         drawVehicle(renderer, &vehicle3);
         drawVehicle(renderer, &vehicle4);
+        drawVehicle(renderer, &vehicle5);
+        drawVehicle(renderer, &vehicle6);
 
         SDL_RenderPresent(renderer);
         SDL_Delay(30);
